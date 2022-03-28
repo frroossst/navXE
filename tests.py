521bcdd988a396d86fb541814d7eee0c322de18d
home <adhyanpatel@gmail.com>
2022-03-28 11:24:26 -0600

    added traversal algorithms and tests

--- tests.py
+++ tests.py
@@ -1,3 +1,12 @@
 import unittest
+from traverse import Path
+
+
+# Checkign if BFS works
+test = {"a" : ["b","c"], "b" : [], "c" : ["d","e"], "d" : [], "e" : []}
+expected = ["a","b","c","d","e"]
+result = Path.BFS(test,"a")
+if result != expected:
+    print(f"BFS test failed; Path.BFS() \nExpected : {expected}\nGot : {result} ")
 
 
--- traverse.py
+++ traverse.py
@@ -3,8 +3,26 @@ class Path():
     def __init__(self,graph):
         pass
 
-    def DFS():
-        pass
+    @classmethod
+    def BFS(self,graph,node):
+        # node is the starting position
+        visited = []
+        queue = []
+        visited.append(node)
+        queue.append(node)
 
-    def BFS():
-        pass
+        while queue:
+            s = queue.pop(0)
+            for x in graph[s]:
+                if x not in visited:
+                    visited.append(x)
+                    queue.append(x)
+        return visited
+
+    @classmethod
+    def DFS(self,graph,node,visited):
+        if node not in visited:
+            visited.append(node)
+            for x in graph[node]:
+                Path.DFS(graph,x,visited)
+        return visited
