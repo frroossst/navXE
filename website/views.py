e38b1326a0b90d83420ff6ec4feb9fed3053272f
home <adhyanpatel@gmail.com>
2022-04-07 11:42:08 -0600

    flask pages setup

--- website/views.py
+++ website/views.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return "<h1> Test Home </h1>"
+    return render_template("base.html")
+
+@views.route("/navigate",methods=["GET","POST"])
+def navigate():
+    return "This is the navigation page"
+
+@views.route("/maps")
+def maps():
+    return "Look at all the user generated maps"
+
+@views.route("/create")
+def create():
+    return "This is where you create a new graph"
+
+@views.route("/code")
+def code():
+    return "GitHub link"
+
+@views.route("/login")
+def login():
+    return "Login Page"
+
+@views.route("/docs")
+def docs():
+    return "Documentation"
