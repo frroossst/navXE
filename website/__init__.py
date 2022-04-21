051490b8d149dc6f53a315ca20e797822dad8332
home <adhyanpatel@gmail.com>
2022-04-21 10:27:24 -0600

    more minor changes

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,17 @@
+from modulefinder import packagePathMap
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import secrets
+
+db = SQLAlchemy()
+DB_NAME = "graphs.db"
 
 def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secrets.token_hex(16)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
 
