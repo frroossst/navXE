c9c8c731efd7f3e9cda21db00d35d3b6982bc46f
home <adhyanpatel@gmail.com>
2022-05-01 20:18:21 -0600

    0.3.0 - gitignore --security

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_cors import CORS
 import secrets
+import os
 
 db = SQLAlchemy()
 DB_NAME = "graphs.db"
@@ -12,8 +13,8 @@ def create_app():
     
     app.config['SECRET_KEY'] = secrets.token_hex(16)
   
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://utftpzpmowwrbj:c8043af3092a13f20f7ea6e27e1df4bd4f19a7cac9ff97898174169e4b318ff9@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d8618c3f7auie6"
-   
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
     db.init_app(app)
     
     CORS(app,resources={r'/*' : {'origins' : '*'}})
