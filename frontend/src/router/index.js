99493079700e8a4293be2c30a2787221d69a7e0a
home <adhyanpatel@gmail.com>
2022-05-05 00:27:29 -0600

    0.4.1 - fixed routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Form from '../components/Forms.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Form
   },
   {
     path: '/about',
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,10 +4,12 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
+import Forms from '@/components/Forms.vue'
 
 export default {
   name: 'HomeView',
   components: {
+      Forms
   }
 }
 </script>
