7d4a69e3cdca53a666e1e80a40bc2f9077e2cf3a
home <adhyanpatel@gmail.com>
2022-05-05 17:13:14 -0600

    0.4.3 - added About page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+       <a class="link-route" href="https://github.com/frroossst/navXE/blob/master/DOCUMENTATION">Documentation</a>  
+      <br>
+       <a class="link-route" href="https://github.com/frroossst/navXE">GitHub</a>  
+      <br>
+       <a class="link-route" href="https://github.com/frroossst/navXE/issues">Report</a> 
+  </div>
+  <div class="about">
+    <p class="link-route">
+      navXE is a local navigation app that relies on locally placed QR codes to get current location of the users 
+      and lead them to their destination. This is a great add on university campuses, office buildings etc. <br> <br>
+
+      This website can be used as a website or an app on your phone (PWA) 
+    </p>
   </div>
 </template>
+
+<style scoped>
+
+  .link-route {
+      margin: 30px 30px;
+      text-align: center;
+      padding: 15px 15px;
+      font-size: 16px;
+      list-style: none;
+      border: 15px 15px;
+      display: inline-block;
+  }
+
+  .about{
+      background-color: #f5f5f5;
+      max-width: 420px;
+      margin: 30px auto;
+  }
+
+</style>
